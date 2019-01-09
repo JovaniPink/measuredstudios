65b9a181692f7ae052e03d4b9db182604480d1be
Jovani Pink <jovanirosado@me.com>
2019-01-09 12:34:05 -0500

    Fixed the broken gatsby-plugin-manifest build

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,51 +21,14 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         // This path is relative to the root of the site.
-        icon: [
-          {
-            "src": "src/assets/favicon/android-icon-36x36.png",
-            "sizes": "36x36",
-            "type": "image/png",
-            "density": "0.75"
-          },
-          {
-            "src": "src/assets/favicon/android-icon-48x48.png",
-            "sizes": "48x48",
-            "type": "image/png",
-            "density": "1.0"
-          },
-          {
-            "src": "src/assets/favicon/android-icon-72x72.png",
-            "sizes": "72x72",
-            "type": "image/png",
-            "density": "1.5"
-          },
-          {
-            "src": "src/assets/favicon/android-icon-96x96.png",
-            "sizes": "96x96",
-            "type": "image/png",
-            "density": "2.0"
-          },
-          {
-            "src": "src/assets/favicon/android-icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png",
-            "density": "3.0"
-          },
-          {
-            "src": "src/assets/favicon/android-icon-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "density": "4.0"
-          }
-        ],
+        icon: 'src/assets/favicon/website-icon.png',
       },
     },
     {
       resolve: `gatsby-plugin-sitemap`
     },
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: `gatsby-plugin-robots-txt`,
       options: {
         host: 'https://www.measuredstudios.com',
         sitemap: 'https://www.measuredstudios.com/sitemap.xml',
